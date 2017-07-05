84e9101a6d5273fd2a7cf386940df44011fb4991
Stephen Knott <stephenk@sbsintellect.co.za>
2017-07-05 21:28:13 +0200

    Add stuff	base

---
+++ StravaConsole/StravaDb/dbo/Tables/Activity.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Activity] (
+    [ActivityId]       INT             NOT NULL,
+    [Name]             VARCHAR (255)   NULL,
+    [Date]             DATE            NULL,
+    [Distance]         DECIMAL (18, 2) NULL,
+    [ActivityType]     VARCHAR (50)    NULL,
+    [GearId]           VARCHAR (50)    NULL,
+    [AverageCadence]   DECIMAL (18, 2) NULL,
+    [AverageHeartRate] DECIMAL (18, 2) NULL,
+    [TimeStarted]      DATETIME        NULL,
+    [TimeEnded]        DATETIME        NULL,
+    [ElevationGain]    DECIMAL (18, 2) NULL,
+    CONSTRAINT [PK_Activity] PRIMARY KEY CLUSTERED ([ActivityId] ASC)
+);
+
---
+++ StravaConsole/StravaDb/dbo/Tables/Gear.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Gear] (
+    [GearId]      VARCHAR (50) NOT NULL,
+    [Description] VARCHAR (50) NULL,
+    CONSTRAINT [PK_Gear] PRIMARY KEY CLUSTERED ([GearId] ASC)
+);
+
